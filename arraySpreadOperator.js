b41b8ff8becdc7ac5cbb67731683f342117c704a
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-23 13:35:20 +0200

    Object spread operator vs object.assign

--- arraySpreadOperator.js
+++ arraySpreadOperator.js
@@ -4,3 +4,17 @@ let list = [5, 6, 7]
 
 const cloned = [...data, ...list, 8, 9]
 console.log(cloned);
+
+
+const obj = {
+    id: 123,
+    name: 'name'
+}
+
+const preferences = { color: 'red', language: 'javascript'};
+const merged = { ...obj, ...preferences, pet: 'dog'};
+//const newClone = Object.assign({}, obj, preferences);
+merged.id = 456;
+
+console.log(obj);
+console.log(merged);
