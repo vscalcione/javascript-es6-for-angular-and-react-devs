8c62d18e522f30a3bf0f1f2248fce67cc0328564
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-23 14:51:06 +0200

    Fixing destructuringObjects.js and added arrow functions

---
+++ arrowFunction.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b
+const divide = (a, b) => a / b;
+const pow = (a) => a * a;
+const greetings = () => console.log('hello');
+
+console.log("Result of sum is: ", add(3, 4));
+console.log("Result of division is: ", divide(4, 2));
+console.log("Pow is: ", pow(4));
+
+greetings();
--- destructuringObject.js
+++ destructuringObject.js
@@ -1,50 +1,60 @@
-const user = {
-    first: 'first',
-    last: 'last',
-    preference: 'red'
-};
+const first = () => {
+    const user = {
+        first: 'first',
+        last: 'last',
+        preference: 'red'
+    };
     
-// const first = user.first;
-// const last = user.last;
-//const { first: f, last: l } = user;
+    // const first = user.first;
+    // const last = user.last;
+    //const { first: f, last: l } = user;
 
-let { first: f, preference: pref = 'black'} = user;
-if (!pref) {
-    pref = 'black';
+    let { first: f, preference: pref = 'black'} = user;
+    if (!pref) {
+        pref = 'black';
+    }
+    console.log(`${f} ${pref}`);
 }
-console.log(`${f} ${pref}`);
 
-const user2 = {
-    name: 'name',
-    surname: 'surname',
-    profile: {
-        color: 'red',
-        location: {
-            lat: 15, lng: 12
-        },
-    }
-};
-
-const { profile: { location: { lat, lng, zoom = 5 } } } = user2;
-console.log(lat, lng, zoom);
-
-
-const user3 = {
-    name: 'name', 
-    surname: 'surname',
-    profile: {
-        color: 'red',
-        location: { lat: 15, lng: 12 },
-    }
-};
-
-const {
-    profile: {
-        location: {
-            lat: lt, lng: lg, zoom1 = 5
+const second = () => {
+    const user2 = {
+        name: 'name',
+        surname: 'surname',
+        profile: {
+            color: 'red',
+            location: {
+                lat: 15, lng: 12
+            },
         }
-    }
-} = user3;
+    };
+    
+    const { profile: { location: { lat, lng, zoom = 5 } } } = user2;
+    console.log(lat, lng, zoom);
+}
 
-const params = { lt, lg, zoom1 };
-console.log(params);
+const third = () => {
+    const user3 = {
+        name: 'name', 
+        surname: 'surname',
+        profile: {
+            color: 'red',
+            location: { lat: 15, lng: 12 },
+        }
+    };
+    
+    const {
+        profile: {
+            location: {
+                lat: lt, lng: lg, zoom1 = 5
+            }
+        }
+    } = user3;
+    
+    const params = { lt, lg, zoom1 };
+    console.log(params);
+    
+}
+
+first();
+second();
+third();
