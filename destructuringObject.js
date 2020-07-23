05e7fc555fffbee394ad3ecaf2db482a276c0637
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-23 13:14:07 +0200

    Comple destructuring

--- destructuringObject.js
+++ destructuringObject.js
@@ -1,22 +1,50 @@
-const firstPart = () => {
-    const user = {
-        first: 'first',
-        last: 'last',
-        preference: 'red'
-    };
+const user = {
+    first: 'first',
+    last: 'last',
+    preference: 'red'
+};
     
-    // const first = user.first;
-    // const last = user.last;
-    //const { first: f, last: l } = user;
-    
-    let { first: f, preference: pref = 'black'} = user;
-    if (!preference) {
-        preference = 'black';
+// const first = user.first;
+// const last = user.last;
+//const { first: f, last: l } = user;
+
+let { first: f, preference: pref = 'black'} = user;
+if (!pref) {
+    pref = 'black';
+}
+console.log(`${f} ${pref}`);
+
+const user2 = {
+    name: 'name',
+    surname: 'surname',
+    profile: {
+        color: 'red',
+        location: {
+            lat: 15, lng: 12
+        },
     }
-    console.log(`${f} ${pref}`);
-}
+};
 
-const secondPart = () => {
-    
-}
+const { profile: { location: { lat, lng, zoom = 5 } } } = user2;
+console.log(lat, lng, zoom);
 
+
+const user3 = {
+    name: 'name', 
+    surname: 'surname',
+    profile: {
+        color: 'red',
+        location: { lat: 15, lng: 12 },
+    }
+};
+
+const {
+    profile: {
+        location: {
+            lat: lt, lng: lg, zoom1 = 5
+        }
+    }
+} = user3;
+
+const params = { lt, lg, zoom1 };
+console.log(params);
